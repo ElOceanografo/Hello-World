dca026a80a8f1f861269ae3398b0a24ca543bce5
Sam Urmy <sam.urmy@gmail.com>
2011-11-10 01:27:11 -0800

    Added unit testing folder and script

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+# hello_world.py
+
+def greeting():
+    return "Hello, World!"
+    
+if __name__ == '__main__':
+    print greeting()
---
+++ tests/test_hello.py
@@ -0,0 +1,5 @@
+# test_hello.py
+from hello_world import *
+
+def test_greeting():
+    assert greeting() == 'Hello, World!'
